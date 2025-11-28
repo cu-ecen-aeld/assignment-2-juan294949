966d48265d51010203f24d47d505d29e4ba3d9b1
juan294949 <juan2949@yahoo.com>
2025-11-27 22:19:35 -0600

    updating

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -34,43 +34,12 @@ fi
 
 # Create a new file with the in path writefile with content writestr.
 
-if [ $(echo $writestr > $writefile) -eq 0 ]; then
+if [[ $(echo $writestr > $writefile) -eq 0 ]]; then
 
-  echo " SUCCESS "
   exit 0
+
 else
 
-  echo " FAIL: A problem occured trying to write at $writefile"
   exit 1
+
 fi
-# Overwrite any existing file  and creating a new path if it does not exist.
-# If file can not be created, exit with error 1 and print statement.
-
-function getX ()
-{
-  # Argument 1 path to directory.
-  filesdir=$1
-  # Find total number of files and subdirectories at "filesdir"
-  find $filesdir -type f | cat > ./X.txt
-  # Obtain the number of lines that were redirected to the X.txt file"
-  X=$(wc -l ./X.txt | cut -d' ' -f1)
-  rm ./X.txt
-  echo "$X"
-}
-
-function getY()
-{
-  # Argument 2 string to search.
-  filesdir=$1
-  searchstr=$2
-  grep -r  $searchstr $filesdir/* | cat > ./Y.txt
-  # number of matching lines found in respective files.
-  Y=$(wc -l ./Y.txt | cut -d' ' -f1)
-  rm ./Y.txt
-  echo $Y
-}
-
-# X:  Is the number of files in the directory and all subdirectories. 
-# Y:  Is the number of matching lines found in respective files. matching lines dictated by SEARCHSTR.
-
-echo "The number of files are $(getX $1 ) and the number of matching lines are $(getY $1 $2) "
